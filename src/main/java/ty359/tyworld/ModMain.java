12379d9a7068ebd5ec753d66edec2c731fe805b4
TaoYe <qq632602549@gmail.com>
2019-02-13 21:07:18 +0800

    初始化项目： 指定了项目名称，包名称等基本信息。

--- src/main/java/ty359/tyworld/ModMain.java
+++ src/main/java/ty359/tyworld/ModMain.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package ty359.tyworld;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,13 +7,9 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = "tyworld", useMetadata = true)
+public class ModMain
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
-
     private static Logger logger;
 
     @EventHandler
@@ -25,7 +21,7 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
+        logger.info("ty World Start!");
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
